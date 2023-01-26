47ff83f211c29dd202947e76f889d524902d429c
CodingKen02 <71536021+CodingKen02@users.noreply.github.com>
2023-01-25 22:49:43 -0600

    Initial commit

---
+++ BreakTest.java
@@ -0,0 +1,33 @@
+/*
+ * Name: Kennedy Keyes
+ * Date: April 17, 2021
+ * Name of Program: BreakTest.java
+ * Description: This program will display a break statement exiting a for 
+statement.
+ */
+package breaktest;
+
+/**
+ *
+ * @author codingken
+ */
+public class BreakTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int count; // control variable also used after loop terminates
+        
+        for (count = 1; count <= 10; count++) { // loop 10 times
+            if (count == 5) {
+                break; // terminates loop if count is 5
+            }
+            
+            System.out.printf("%d ", count);
+        }
+        
+        System.out.printf("%nBroke out of loop at count = %d%n", count);
+    }
+    
+}
